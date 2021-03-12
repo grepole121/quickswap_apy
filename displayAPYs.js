e74ec4bed079c6bbb4bf84aa6bc581cfd8ce2dba
George Repole <grepole121@users.noreply.github.com>
2021-03-12 04:17:14 +0000

    added disconnect variable

--- displayAPYs.js
+++ displayAPYs.js
@@ -5,6 +5,9 @@ var tvl = [];
 var quickday = [];
 var apy = [];
 
+//Change disconnected from 0 to 1 if runnning the script when disconnected
+var disconnected = 0;
+
 var Httpreq = new XMLHttpRequest();
 Httpreq.open("GET","https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=0x6c28aef8977c9b773996d0e8376d2ee379446f2f&vs_currencies=usd",false);
 Httpreq.send(null);
@@ -17,7 +20,7 @@ var v1 = parseFloat(values[0])
 var v2 = parseFloat(values[1])
 price =  v1 + (v2/100)
 
-for (i = 2; i < (x.length)-2; i+=6) {
+for (i = 2-disconnected; i < (x.length)-2; i+=6) {
 	a = x[i].textContent
 	a = a.replace(",","")
 	a = a.replace(",","")
@@ -26,7 +29,7 @@ for (i = 2; i < (x.length)-2; i+=6) {
   tvl.push(a);
 }
 
-for (i = 4; i < (x.length)-2; i+=6) {
+for (i = 4-disconnected; i < (x.length)-2; i+=6) {
 	a = x[i].textContent
 	a = a.replace(" QUICK / day", "")
 	a = parseFloat(a)
@@ -41,6 +44,6 @@ for (i=0; i < tvl.length; i++){
 }
 
 for (i=6; i < x.length; i+=6){
-	x[i].textContent = apy[(i-6)/6]
-	x[i-1].textContent = "Current APY: "
+	x[i-disconnected].textContent = apy[(i-6)/6]
+	x[i-1-disconnected].textContent = "Current APY: "
 }
