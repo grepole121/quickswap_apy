998e248b4ab1c7eb1148f8ba32d7890046870ceb
George Repole <grepole121@users.noreply.github.com>
2021-03-14 21:28:08 +0000

    fixed not working if total deposits were in eth

--- displayAPYs.js
+++ displayAPYs.js
@@ -16,10 +16,16 @@ if ( (wallet[0].textContent == "Switch to Matic") || (wallet[0].textContent == "
 }
 
 // Get price from CoinGecko API
-var Httpreq = new XMLHttpRequest();
-Httpreq.open("GET","https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=0x6c28aef8977c9b773996d0e8376d2ee379446f2f&vs_currencies=usd",false);
-Httpreq.send(null);       
-var price = JSON.parse(Httpreq.responseText)["0x6c28aef8977c9b773996d0e8376d2ee379446f2f"]["usd"];
+var quick_request = new XMLHttpRequest();
+quick_request.open("GET","https://api.coingecko.com/api/v3/simple/price?ids=quick&vs_currencies=usd",false);
+quick_request.send(null);       
+var quick_price = JSON.parse(quick_request.responseText)["quick"]["usd"];
+
+var eth_request = new XMLHttpRequest();
+eth_request.open("GET","https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd",false);
+eth_request.send(null);       
+var eth_price = JSON.parse(eth_request.responseText)["ethereum"]["usd"];
+
 
 for (i = 2-disconnected; i < (x.length)-2; i++) {
 	// Get the text of each element in the class
@@ -27,25 +33,33 @@ for (i = 2-disconnected; i < (x.length)-2; i++) {
 	// Remove any text from TVL
 	a = a.replace(",","")
 	a = a.replace(",","")
-	a = a.replace("$","")
+	
 	// Remove any text from QUICK/day
 	a = a.replace(" QUICK / day", "")
-	// Convert string to a float
-	a = parseFloat(a)
+	
 	// Check if the previous element was total deposits or pool rate
 	// If total deposits then this element is the total value locked
 	// Otherwise if pool rate then this element is the QUICK per day
 	// Then add that value to the corresponding array
 	if (x[i-1].textContent == " Total deposits"){
+		if (a[0] == "$"){
+			a = a.replace("$","")
+			a = parseFloat(a)
+		}else{
+			a = a.replace("ETH","")
+			a = parseFloat(a)
+			a *= eth_price
+		}
 		tvl.push(a);
 	}else if (x[i-1].textContent == " Pool rate "){
+		a = parseFloat(a)
 		quickday.push(a)
 	}
 }
 
 // Calculate the APY for each TVL and QUICK per day pair
 for (i=0; i < tvl.length; i++){
-	a = (365 * 100 * quickday[i]*price) / tvl[i]
+	a = (365 * 100 * quickday[i]*quick_price) / tvl[i]
 	a = Math.round(a*100)/100
 	a = a.toString().concat("%")
 	apy.push(a)
