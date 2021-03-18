3e33b570cc3d1626d46b8333350b4ab61b467010
George Repole <grepole121@users.noreply.github.com>
2021-03-18 21:20:09 +0000

    Now appends APY instead of overwriting "Status"

--- displayAPYs.js
+++ displayAPYs.js
@@ -1,12 +1,11 @@
 // Declare variables
 var x = document.getElementsByClassName("sc-kkGfuU WmMZl css-8626y4");
+var p = document.getElementsByClassName("sc-gVLVqr iSoxiC");
 var i;
 var a;
 var tvl = [];
 var quickday = [];
-var apy = [];
-var apy_positon = 0;
-
+var apy;
 
 // Get QUICK price from CoinGecko API
 var quick_request = new XMLHttpRequest();
@@ -28,7 +27,6 @@ eth_request.open(
 eth_request.send(null);
 var eth_price = JSON.parse(eth_request.responseText)["ethereum"]["usd"];
 
-
 // Loop through elements to get total value locked and QUICK per day
 for (i = 1; i < x.length - 2; i++) {
   // Get the text of each element in the class
@@ -46,8 +44,8 @@ for (i = 1; i < x.length - 2; i++) {
   // Then add that value to the corresponding array
   if (x[i - 1].textContent == " Total deposits") {
     // Check if dollars or ETH
-	// If ETH multiply by ETH price
-	
+    // If ETH multiply by ETH price
+
     if (a[0] == "$") {
       a = a.replace("$", "");
       a = parseFloat(a);
@@ -56,33 +54,43 @@ for (i = 1; i < x.length - 2; i++) {
       a = parseFloat(a);
       a *= eth_price;
     }
-	// Append to the total value locked array
+    // Append to the total value locked array
     tvl.push(a);
   } else if (x[i - 1].textContent == " Pool rate ") {
-	// Convert from string to float
+    // Convert from string to float
     a = parseFloat(a);
-	// Append to the total value locked array
+    // Append to the total value locked array
     quickday.push(a);
   }
 }
 
-// Calculate the APY for each TVL and QUICK per day pair
+// Display the APY on the screen
 for (i = 0; i < tvl.length; i++) {
-  a = (365 * 100 * quickday[i] * quick_price) / tvl[i];
-  a = Math.round(a * 100) / 100;
-  a = a.toString().concat("%");
-  apy.push(a);
-}
-// Display the APY on the screen overwriting the status as it isn't important data
-for (i = 2; i < x.length; i++) {
-	// Check if the previous element has the text "Status" or "Current APY:" so it knows to overwrite
-  if (
-    x[i - 1].textContent == " Status " ||
-    x[i - 1].textContent == "Current APY: "
-  ) {
-	  // Replace the text with the current APY
-    x[i].textContent = apy[apy_positon];
-    x[i - 1].textContent = "Current APY: ";
-    apy_positon++;
+  // Calculate the APY
+  apy = (365 * 100 * quickday[i] * quick_price) / tvl[i];
+  apy = Math.round(apy * 100) / 100;
+  apy = apy.toString().concat("%");
+
+  // Creates a new element with Current APY
+  var node = document.createElement("div");
+  node.className = "sc-gqjmRU sc-jTzLTM sc-fjdhpX hwjYkd";
+
+  var textnode = document.createElement("div");
+  textnode.className = "sc-kkGfuU WmMZl css-8626y4";
+  textnode.append("Current APY: ");
+
+  var apynode = document.createElement("div");
+  apynode.className = "sc-kkGfuU WmMZl css-8626y4";
+  apynode.append(apy);
+
+  node.appendChild(textnode);
+  node.appendChild(apynode);
+
+  // Checks if element has been appended before
+  // If so delete that element
+  if (p[i].lastChild.firstChild.textContent == "Current APY: ") {
+    p[i].removeChild(p[i].lastChild);
   }
+  // Append the element with APY
+  p[i].append(node);
 }
